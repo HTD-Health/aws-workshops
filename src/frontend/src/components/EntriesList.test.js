7d38066d47726c36845ea2bf49985cdbdff593d8
JakubZygmunt <jakub.zygmunt@htdevelopers.com>
2020-11-03 16:34:17 +0100

    Format code with prettier

--- src/frontend/src/components/EntriesList.test.js
+++ src/frontend/src/components/EntriesList.test.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import {EntriesList} from './EntriesList';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { EntriesList } from "./EntriesList";
 
-describe('Entries list component tests', () => {
-    const onChange = jest.fn();
-    const onRemove = jest.fn();
-    const createEntry = (id = 0) => ({
-        id: id.toString(10),
-        completed: true,
-        body: `test-body=${id}`
+describe("Entries list component tests", () => {
+  const onChange = jest.fn();
+  const onRemove = jest.fn();
+  const createEntry = (id = 0) => ({
+    id: id.toString(10),
+    completed: true,
+    body: `test-body=${id}`,
+  });
 
-    })
-
-    it('renders entry', () => {
-        const entry = createEntry();
-        const { getByTestId } = render(<EntriesList onChange={onChange} onRemove={onRemove} entries={[entry]}  />);
-        expect(getByTestId(entry.id).textContent).toBe(entry.body);
-    });
-
+  it("renders entry", () => {
+    const entry = createEntry();
+    const { getByTestId } = render(
+      <EntriesList onChange={onChange} onRemove={onRemove} entries={[entry]} />
+    );
+    expect(getByTestId(entry.id).textContent).toBe(entry.body);
+  });
 });
